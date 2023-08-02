0da81eb8683388a74951e9d89c9798f6e9f03a47
DANIEL ANDRADE SILVA <daniel_silva37@estudante.sesisenai.org.br>
2023-08-01 21:55:36 -0300

    Estrutura de dados aula 02

---
+++ RevisaoJava/src/Main.java
@@ -0,0 +1,84 @@
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner _reader = new Scanner(System.in);
+    public static void main(String[] args) {
+        somaDosElementosDoVetor();
+        _reader.close();
+    }
+
+    public static void inverterVetor(){
+        int[] vetor = { 10, 9, 8, 7, 6}, auxiliar = new int[5];
+        int lenght = vetor.length;
+
+        printLn("Vetor de entrada: " + vetorToString(vetor));
+
+        for(int i = 0; i < lenght; i++){
+            auxiliar[(lenght - i) - 1] = vetor[i];
+        }
+
+        printLn("Vetor invertido: " + vetorToString(auxiliar));
+    }
+
+    public static void somaDosElementosDoVetor(){
+        int[] vetor = new int[10];
+        int soma = 0;
+
+        printLn("Digite 10 números inteiros e saiba sua soma.");
+
+        for(int i = 0; i < 10; i++){
+            printLn("Digite o " + (i + 1) + "º número: ");
+            vetor[i] = validateInteger(_reader.next());
+            soma = soma + vetor[i];
+        }
+
+        printLn("A soma dos elementos do vetor " + vetorToString(vetor) + " é: " + soma + ".");
+    }
+
+    public static void imprimirPares(){
+
+    }
+
+    public static String vetorToString(int[] vetor){
+        int lenght = vetor.length;
+
+        StringBuilder output = new StringBuilder();
+
+        for(int i = 0; i < lenght; i++) {
+            if(i == 0){
+                output.append("{").append(vetor[i]).append(", ");
+            }
+            else if (i == (lenght-1)){
+                output.append(vetor[i]).append("}");
+            }
+            else {
+                output.append(vetor[i]).append(", ");
+            }
+        }
+        return output.toString();
+    }
+
+    public static Integer validateInteger(String input){
+        int outInt = 0;
+        boolean controlFlow = true;
+
+        while(controlFlow){
+            try{
+                outInt = Integer.parseInt(input);
+                controlFlow = false;
+            }
+            catch(NumberFormatException exception){
+                printLn("Caracter inválido, tente novamente.");
+                printLn("Use apenas números inteiros");
+                input = _reader.next();
+            }
+        }
+        return outInt;
+    }
+
+    public static void printLn(String text){
+        System.out.println(text);
+    }
+}
+
